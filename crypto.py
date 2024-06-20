3459781ef78b9d0c08dc11226bf86cfba55c8560
rsvgsng <111951452+rsvgsng@users.noreply.github.com>
2024-06-20 18:27:45 +0545

    Update crypto.py v5

--- crypto.py
+++ crypto.py
@@ -2,7 +2,7 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 
 
-#This is version 4 
+#This is version 5 from github
 private_key = rsa.generate_private_key(
     public_exponent=65537,
     key_size=2048,
