0d4815ab8bfe365f379837f326b5dc8fc7aba15d
Rishav Ghising <ghisingrishav@gmail.com>
2024-06-20 17:48:44 +0545

    Added line

---
+++ crypto.py
@@ -0,0 +1,45 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+)
+
+public_key = private_key.public_key()
+
+pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+with open('private_key.pem', 'wb') as f:
+    f.write(pem)
+
+pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+with open('public_key.pem', 'wb') as f:
+    f.write(pem)
+
+message = b"Hello, world!"
+cipher_text = public_key.encrypt(
+    message,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+print(f"Encrypted message: {cipher_text}")
+
+plain_text = private_key.decrypt(
+    cipher_text,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+print(f"Decrypted message: {plain_text.decode()}")
